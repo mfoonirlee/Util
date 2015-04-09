8c3bd2a2bd2c6816f96fea999237d38e6dd80b7e
mfoonirlee <745594100@163.com>
2015-04-09 16:42:16 +0800

    Update Util.js
    add a new method

--- Util.js
+++ Util.js
@@ -16,6 +16,26 @@
     */
     isEmail : function (val) {
         return /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(val);
+    },
+    /**
+     * @description: 将传入的某个val中的非法字符(非a-zA-Z0-9)过滤掉(主要为了兼容移动端输入法导致的常规过滤失效的问题)
+     * @param: string
+     */
+    filterWord : function(val) {
+        var valArr = val.split(''),
+            valCodeArr, res = val;
+        
+        valArr.forEach(function(v, i){
+            valCodeArr.push(v.charCodeAt(0));
+        });
+        
+        if(val.match(/[^0-9a-z]/i) && valCodeArr.indexOf(8198) === -1){
+            res = val.replace(/[^0-9a-z]/img, '');
+            if(res.match(/\s/)){
+                res = res.replace(/\s/mg, '');
+            }
+        }
+        return res;
     }
   };
   //非模块加载的环境下，在window下给予该类的一个引用
