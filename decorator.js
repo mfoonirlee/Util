d79bde5518ae2bc93c635de0a792c6edc365891a
mfoonirlee <745594100@163.com>
2017-03-02 14:23:03 +0800

    Update decorator.js

--- decorator.js
+++ decorator.js
@@ -6,8 +6,8 @@ export var decorateLogger = (type) =>{
 	return (target, name, descriptor) => {
 		const method = descriptor.value;
 		descriptor.value = function(...args) {
-      //other logger code			
-      console.info(`logger: ${type}`);
+      			//other logger code			
+			console.info(`logger: ${type}`);
 			let ret;
 			try{
 				ret = method.apply(target, args);
