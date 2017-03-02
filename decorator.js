7c47cdfe1ca2916eba825ec30605ffc489fbf3c3
mfoonirlee <745594100@163.com>
2017-03-02 14:24:54 +0800

    Update decorator.js

--- decorator.js
+++ decorator.js
@@ -3,18 +3,18 @@
 * @param: 需要装饰的tpye
 */
 export var decorateLogger = (type) =>{
-	return (target, name, descriptor) => {
-		const method = descriptor.value;
-		descriptor.value = function(...args) {
-      			//other logger code			
-			console.info(`logger: ${type}`);
-			let ret;
-			try{
-				ret = method.apply(target, args);
-			}catch(e){
-				console.error(e);
-			}
-			return ret;
-		}
-	}
+    return (target, name, descriptor) => {
+        const method = descriptor.value;
+        descriptor.value = function(...args) {
+            //other logger code			
+            console.info(`logger: ${type}`);
+            let ret;
+            try{
+                ret = method.apply(target, args);
+            }catch(e){
+                console.error(e);
+            }
+            return ret;
+        }
+    }
 }
